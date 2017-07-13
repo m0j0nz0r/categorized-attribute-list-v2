ac7c32b7651d48f99a79b65b12bd820a21ceeb59
Victor Hernandez <victor.hernandez@jobsity.com>
2017-07-13 16:30:49 -0500

    Enimerations list functionality

--- src/Components/Categories/AttributeList/Attribute/Enumerations/index.jsx
+++ src/Components/Categories/AttributeList/Attribute/Enumerations/index.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 
 export default (locals) => {
-  console.log(locals, locals.add.click);
   const addItem = () => {
     const value = this.input.value;
     if (value) {
-      locals.add.click();
+      locals.onChange(
+        [...locals.value, ...[value]],
+        locals.items.map(i => i.key),
+        [...locals.path, ...['']],
+        'add',
+      );
+      this.input.value = '';
     }
   };
-
   return (
     <div>
       <input ref={(input) => { this.input = input; }} type="string" />
       <button type={locals.add.type} onClick={addItem}>{locals.add.label}</button>
-      <ul>
-        {locals.items.map(i => <li key={i.key}>{i.input.value}</li>)}
-      </ul>
+      {locals.items.map(
+        (i, index) => (
+          <div key={i.key}>
+            <button type={i.buttons[0].type} onClick={i.buttons[0].click}>x</button>
+            {locals.value[index]}
+          </div>
+        ),
+      )}
     </div>
   );
 };
